2c2fc09c8df99fa1d9af76ba1757b339cf38f555
hrytsyuka <anastasiyah@amco.me>
2018-04-06 17:25:57 -0700

    validate product

--- spec/views/products/index.html.haml_spec.rb
+++ spec/views/products/index.html.haml_spec.rb
@@ -1,28 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe "products/index", type: :view do
-  before(:each) do
-    assign(:products, [
-      Product.create!(
-        :title => "Title",
-        :description => "MyText",
-        :image_url => "rails5.png",
-        :price => "9.99"
+  
+ before(:each) do
+  assign(:products, [
+    Product.create!(
+      :title => "Book title 1",
+      :description => "Book description",
+      :image_url => "rails5.png",
+      :price => "9.99"
       ),
-      Product.create!(
-        :title => "Title",
-        :description => "MyText",
-        :image_url => "rails5.png",
-        :price => "9.99"
+    Product.create!(
+      :title => "Book title 2",
+      :description => "Book description",
+      :image_url => "rails5.png",
+      :price => "9.99"
       )
     ])
-  end
+end
 
-  it "renders a list of products" do
-    render
-    assert_select "tr>td>h2", :text => "Title".to_s, :count => 2
-    assert_select "tr>td>p", :text => "MyText".to_s, :count => 2
-    assert_select "tr>td>img", :image_url => "/assets/rails5.png", :count => 2
-    assert_select "tr>td", :text => "9.99".to_s, :count => 2
-  end
+it "renders a list of products" do
+  render
+  expect(rendered).to have_selector("tr>td>h2", :text => /Book title/), :count => 2
+  expect(rendered).to have_selector("tr>td>p", :text => /Book description/), :count => 2
+  expect(rendered).to have_selector('img[src*="rails5"]'), :count => 2
+  expect(rendered).to have_selector("tr>td", :text => /9.99/), :count => 2
+end
 end
